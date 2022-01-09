aecc12e91563b42ec6bcd32a1b2f686a88b7394e
chungyau97 <chungyau97@gmail.com>
2022-01-09 17:06:30 +0800

    add db connection setup

---
+++ mvc-tutorial-1/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using mvc_tutorial_1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mvc_tutorial_1.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Catergory { get; set; }
+    }
+}
